cb902bce2df20b455ade931689c6dc32b8c5bf38
Guillaume Gemelas <GuiGui@MacBook-Pro.local>
2023-02-15 16:38:51 +0100

    second commit

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const cors = require("cors");
+const mongoose = require("mongoose");
 require("dotenv").config();
 const axios = require("axios");
 
@@ -21,11 +22,14 @@ app.get("/", async (req, res) => {
 });
 //
 
-//route /comics qui renvoie
+//ROUTE 1: /comics qui renvoie-------------------------------------------------------------------------------------------------------------------------------------------------------
 app.get("/comics", async (req, res) => {
   try {
     const response = await axios.get(
       `https://lereacteur-marvel-api.herokuapp.com/comics?apiKey=${process.env.YOUR_API_KEY}`
+
+      // url avec toutes les clés qu'il faudra renseigner au besoin:
+      //   `https://lereacteur-marvel-api.herokuapp.com/comics?apiKey=${process.env.YOUR_API_KEY}&limit=${limit}&skip=${skip}&title=${title}`
     );
 
     // console.log(response); //renvoie la réponse globlale avec tableau d'objets pour la clé results
@@ -34,11 +38,11 @@ app.get("/comics", async (req, res) => {
     // console.log(response.data.results); //renvoie un tableau de résultats
     // console.log(response.data.results[1].title); //renvoie au titre de l'élément 1 de l'objet results
 
-    //réponse du serveur avec le tableau d'objet global (count, limit et results)
-    // res.status(200).json(response.data);
+    //réponse du serveur avec le tableau d'objet results
+    // res.status(200).json(response.data.results);
 
-    ////réponse du serveur avec le tableau d'objet results
-    res.status(200).json(response.data.results);
+    ////réponse du serveur avec le tableau d'objet  global (count, limit et results)
+    res.status(200).json(response.data);
     //
   } catch (error) {
     res.status(400).json(error.message);
@@ -46,7 +50,7 @@ app.get("/comics", async (req, res) => {
 });
 //
 
-//seconde route characterId:
+//ROUTE 2: characterId:----------------------------------------------------------------------------------------------------------------------------------------------------------------
 app.get("/comics/:characterId", async (req, res) => {
   try {
     const response = await axios.get(
@@ -55,13 +59,56 @@ app.get("/comics/:characterId", async (req, res) => {
     console.log(req.params); //renvoie { characterId: '5fc8ba1fdc33470f788f88b3' }
     console.log(response); //renvoie la réponse globlale avec tableau d'objets pour la clé results
 
-    res.status(200).json(response.config);
+    // res.status(200).json(response.config);
+    res.status(200).json(response.data);
   } catch (error) {
     res.status(400).json(error.message);
   }
 });
 
 //
+
+// ROUTE 3: /characters----------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+app.get("/characters", async (req, res) => {
+  try {
+    const response = await axios.get(
+      `https://lereacteur-marvel-api.herokuapp.com/characters?apiKey=${process.env.YOUR_API_KEY}`
+    );
+    console.log(response);
+    //réponse du serveur avec le tableau d'objet global (count, limit et results)
+    // res.status(200).json(response.data);
+
+    ////réponse du serveur avec le tableau d'objet results
+    res.status(200).json(response.data);
+  } catch (error) {
+    res.status(400).json(error.message);
+  }
+});
+//
+//
+// ROUTE 4: /character/:characterId------------------------------------------------------------------------------------------------------------------------------------------------------
+
+app.get("/character/:characterId", async (req, res) => {
+  try {
+    const response = await axios.get(
+      `https://lereacteur-marvel-api.herokuapp.com/character/:characterId?apiKey=${process.env.YOUR_API_KEY}`
+    );
+    console.log(req.params);
+    console.log(response);
+
+    ////réponse du serveur avec le tableau d'objet results
+    res.status(200).json(req.params);
+  } catch (error) {
+    res.status(400).json(error.message);
+  }
+});
+
+//-------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+app.all("*", (req, res) => {
+  res.status(404).json({ message: "This routes doesn't exist" });
+});
 
 app.listen(3000, () => {
   console.log("Serveur started 😀");
